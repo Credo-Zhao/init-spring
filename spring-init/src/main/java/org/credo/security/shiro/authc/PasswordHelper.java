e288060a7f761569ea415d1f2cd0e7903adf6212
zhaoqian <zhaoqianjava@foxmail.com>
2014-08-10 17:16:57 +0800

    PasswordHelper

---
+++ spring-init/src/main/java/org/credo/security/shiro/authc/PasswordHelper.java
@@ -0,0 +1,15 @@
+/**
+ * <p>Copyright (c) 2014 ZhaoQian.All Rights Reserved.</p>
+ * @author <a href="zhaoqianjava@foxmail.com">ZhaoQian</a>
+ */
+package org.credo.security.shiro.authc;
+
+/**
+ *
+ * @author ZhaoQian
+ * @date: 2014年8月10日
+ */
+public class PasswordHelper
+{
+
+}
